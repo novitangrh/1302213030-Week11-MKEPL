34d685846f25b3a20c18f3a211cf65fa8a8a4b37
novitangrh <95624321+novitangrh@users.noreply.github.com>
2024-05-15 23:30:00 +0700

    Update Driver.java
    Update Sout

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -3,6 +3,8 @@ import src.main.java.Counter;
 public class Driver {
 
 	public static void main(String[] args) {
+
+		System.out.println("Hasil Perhitungan");
 		
 		Counter counter = new Counter();
 		
