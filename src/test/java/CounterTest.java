33295c489454c527c66fcbb1f09ff9ea2dbdfdb8
novitangrh <95624321+novitangrh@users.noreply.github.com>
2024-05-15 23:47:34 +0700

    Update CounterTest.java
    Update testReset

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -16,7 +16,7 @@ public class CounterTest {
 		
 		testCounter.reset();
 		
-		assertEquals(testCounter.getCount(), 0);
+		assertEquals(testCounter.getCount(), 5);
 	}
 
 	@Test
@@ -25,7 +25,7 @@ public class CounterTest {
 		
 		for(int i = 1; i <10; i++) {
 			testCounter.increment();
-			assertEquals(testCounter.getCount());
+			assertEquals(testCounter.getCount(), i);
 		}
 	}
 
